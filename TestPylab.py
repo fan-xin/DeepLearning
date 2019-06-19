126fb3569db9646f3c0e63403da33c2239cbda3a
Xin Fan <xin.x.fan@sony.com>
2019-06-19 18:15:17 +0900

    学习爬虫的基础知识 练习使用爬虫爬取猫眼网的100部电影信息

---
+++ TestPylab.py
@@ -0,0 +1,8 @@
+import pylab
+import numpy as np
+
+x = np.linspace(0,20,1000)
+y = np.sin(x)
+
+pylab.plot(x,y)
+pylab.show()
---
+++ VisualizeDigits.py
@@ -0,0 +1,13 @@
+#from sklearn import datasets
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+print(digits.data.shape)
+
+import pylab as pl
+pl.gray()
+pl.matshow(digits.images[0])
+pl.show()
+
+
+
